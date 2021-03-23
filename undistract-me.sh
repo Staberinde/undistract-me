525215bac7dee5ef31efd36f854ef60b3f1bdc96
Toby Petty <tobypetty1@gmail.com>
2021-03-23 11:30:35 +0000

    Allow undistract-me to be agnostic of its location when sourcing long-running.bash

--- undistract-me.sh
+++ undistract-me.sh
@@ -3,5 +3,6 @@
 # Check for interactive bash and that we haven't already been sourced.
 [ -z "$BASH_VERSION" -o -z "$PS1" -o -n "$last_command_started_cache" ] && return
 
-. /usr/share/undistract-me/long-running.bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+. "$DIR/long-running.bash"
 notify_when_long_running_commands_finish_install
